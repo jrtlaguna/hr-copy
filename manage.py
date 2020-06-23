a21cd966d2593e7c6bb1f27b9e38b8cff9d92cae
Ryan <jrtlaguna@gmail.com>
2020-06-23 14:37:40 +0800

    renamed hrapp project to hr

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hrapp.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hr.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
