fc7f44a51ec0fa3c1d5f247d01871f6b6e3c1ea7
Vincent Jake C <vincent@ingenuity.ph>
2020-08-11 12:35:23 +0800

    models, admin

--- hr/settings.py
+++ hr/settings.py
@@ -60,6 +60,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "employees",
     "users",
+    "leaves",
 ]
 
 SITE_ID = 1
---
+++ leaves/admin.py
@@ -0,0 +1,84 @@
+from django.contrib import admin
+
+from leaves.models import (
+    LeaveAllocation,
+    LeaveApplication,
+    LeaveType,
+)
+
+
+@admin.register(LeaveAllocation)
+class LeaveAllocationAdmin(admin.ModelAdmin):
+    list_display = (
+        "employee",
+        "leave_type",
+        "from_date",
+        "to_date",
+        "count",
+    )
+    list_filter = ("leave_type",)
+    list_select_related = (
+        "employee",
+        "leave_type",
+    )
+    raw_id_fields = (
+        "employee",
+        "leave_type",
+    )
+    search_fields = (
+        "employee__user__first_name",
+        "employee__user__last_name",
+        "employee__nickname",
+    )
+
+
+@admin.register(LeaveApplication)
+class LeaveApplicationAdmin(admin.ModelAdmin):
+    list_display = (
+        "approver",
+        "employee",
+        "leave_type",
+        "from_date",
+        "to_date",
+        "status",
+    )
+    list_filter = (
+        "leave_type",
+        "status",
+    )
+    list_select_related = (
+        "approver",
+        "employee",
+        "leave_type",
+    )
+    raw_id_fields = (
+        "approver",
+        "employee",
+        "leave_type",
+    )
+    search_fields = (
+        "employee__user__first_name",
+        "employee__user__last_name",
+        "employee__nickname",
+        "name",
+    )
+
+
+@admin.register(LeaveType)
+class LeaveTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "is_paid",
+        "is_optional",
+        "is_carry_forwarded",
+        "is_convertible_to_cash",
+        "is_carry_forwarded",
+    )
+    list_filter = (
+        "is_paid",
+        "is_optional",
+        "is_carry_forwarded",
+        "is_convertible_to_cash",
+        "is_carry_forwarded",
+    )
+    search_fields = ("name",)
