aeee71cce725cad50a06502d11da937a767b0fc4
Vincent Jake C <vincent@ingenuity.ph>
2020-08-25 13:43:50 +0800

    leave application permission

--- leaves/api/v1/permissions.py
+++ leaves/api/v1/permissions.py
@@ -3,6 +3,4 @@ from rest_framework.permissions import IsAuthenticated
 
 class LeaveApplicationApproverPermission(IsAuthenticated):
     def has_object_permission(self, request, view, obj):
-        if request.user not in [approver.user for approver in obj.approvers.all()]:
-            return False
-        return True
+        return obj.approvers.filter(user=request.user).exists()
