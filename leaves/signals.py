a77c97e4dae7b63fb34d30f39a33666b03ce5053
Vincent Jake C <vincent@ingenuity.ph>
2020-09-08 18:15:22 +0800

    signals update

--- leaves/signals.py
+++ leaves/signals.py
@@ -1,14 +1,14 @@
 from fieldsignals import post_save_changed
 
 from leaves.models import Holiday, LeaveApplication
+from django.dispatch import receiver
 
 
-def calculate_leave_business_days(sender, instance, created, using, changed_fields=None, **kwargs):
+@receiver(post_save_changed, sender=LeaveApplication)
+def calculate_leave_business_days(sender, instance, created, changed_fields=None, **kwargs):
     fields = ("from_date", "to_date")
-    date_fields_updated = any([f in changed_fields for f in fields])
+    date_fields_updated = any([field in changed_fields for field in fields])
     if created or date_fields_updated:
         business_days_count = Holiday.business_days_count(instance.from_date, instance.to_date)
         instance.count = business_days_count
         instance.save()
-    
-post_save_changed.connect(calculate_leave_business_days, sender=LeaveApplication)
